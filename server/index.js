cbb70ed178652d4000090345103ec07f371c70e5
Farida <belkaidfarida21@gmail.com>
2025-12-16 20:20:45 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const express =require("express");
+const {connectToMongoDB}=require("./database")
+const path = require("path")
+
+const app=express();
+app.use(express.json())
+
+const router = require("./routes")
+app.use("/api", router)
+
+app.use(express.static(path.join(__dirname, "build")))
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "build/index.html"))
+})
+
+const port = process.env.PORT || 5000;
+async function startServer(){
+    await connectToMongoDB();
+    app.listen(port, () =>{
+        console.log( `server is listening on http://localhost:${port}`);
+    })
+}
+startServer();
